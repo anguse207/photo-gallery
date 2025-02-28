934bd5b1b85d5fd9a0fc3314a9e2981547f797bf
Gus <angusedmunds1@gmail.com>
2025-02-28 14:29:44 +0000

    update consts

--- frontend/src/consts.tsx
+++ frontend/src/consts.tsx
@@ -1,5 +1,19 @@
-export const proto = "http://";
-export const url_host = "127.0.0.1:3000";
-export const url_root = proto + url_host;
-export const url_upload = url_root + "/api/upload";
-export const url_ws = "ws://" + url_host + "/api/ws";
+export const url_upload = window.location.origin + "/api/upload";
+export const url_ws = "ws://" + window.location.hostname + "/api/ws";
+
+console.log("url_upload: ", url_upload);
+console.log("url_ws: ", url_ws);
+
+/* Examples...
+window.location.origin
+"https://mail.google.com"
+
+window.location.host
+"mail.google.com"
+
+window.location.hostname
+"mail.google.com"
+
+window.location.protocol
+"https:" 
+*/
