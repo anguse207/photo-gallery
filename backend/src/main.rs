1f6b56bfcebd20c4e67942bf5136c7721819d651
Gus <angusedmunds1@gmail.com>
2025-03-01 15:28:50 +0000

    fmting

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -20,17 +20,15 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    
-
     // Check if the directory exists, if it does, remove it and create a new one
     match std::fs::metadata(PATH_PREFIX.clone()).is_ok() {
         true => {
             std::fs::remove_dir_all(PATH_PREFIX.clone()).unwrap();
             std::fs::create_dir_all(PATH_PREFIX.clone()).unwrap();
-        },
+        }
         false => std::fs::create_dir_all(PATH_PREFIX.clone()).unwrap(),
     }
-        
+
     let state: state::AppState = state::AppState::new();
 
     server::serve(state.clone()).await;
